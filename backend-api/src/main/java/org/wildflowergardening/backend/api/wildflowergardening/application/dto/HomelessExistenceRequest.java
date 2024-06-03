c7a491444a81ece4b4b8ead0ef3762bef21319de
yerimkim <kiel0103@naver.com>
2024-06-03 12:12:06 +0900

    docs: swagger 요청 예시 테스트데이터로 셋팅

--- backend-api/src/main/java/org/wildflowergardening/backend/api/wildflowergardening/application/dto/HomelessExistenceRequest.java
+++ backend-api/src/main/java/org/wildflowergardening/backend/api/wildflowergardening/application/dto/HomelessExistenceRequest.java
@@ -1,11 +1,22 @@
 package org.wildflowergardening.backend.api.wildflowergardening.application.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class HomelessExistenceRequest {
 
+  @NotNull
+  @Schema(example = "1")
   private Long shelterId;
+
+  @NotEmpty
+  @Schema(example = "test_device_id_1")
   private String deviceId;
+
+  @NotEmpty
+  @Schema(example = "홍길동")
   private String homelessName;
 }
--- backend-api/src/main/java/org/wildflowergardening/backend/api/wildflowergardening/application/dto/HomelessTokenRequest.java
+++ backend-api/src/main/java/org/wildflowergardening/backend/api/wildflowergardening/application/dto/HomelessTokenRequest.java
@@ -1,5 +1,6 @@
 package org.wildflowergardening.backend.api.wildflowergardening.application.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -8,14 +9,18 @@ import lombok.Getter;
 public class HomelessTokenRequest {
 
   @NotNull
+  @Schema(example = "1")
   private Long homelessId;
 
   @NotNull
+  @Schema(example = "1")
   private Long shelterId;
 
   @NotEmpty
+  @Schema(example = "홍길동")
   private String homelessName;
 
   @NotEmpty
+  @Schema(example = "test_device_id_1")
   private String deviceId;
 }

6f7d19c8d397b1d9d8e1613486133bbfb64658b7
yerimkim <kiel0103@naver.com>
2024-06-04 18:36:29 +0900

    docs: swagger response description 수정

--- backend-api/src/main/java/org/wildflowergardening/backend/api/wildflowergardening/application/dto/HomelessCountResponse.java
+++ backend-api/src/main/java/org/wildflowergardening/backend/api/wildflowergardening/application/dto/HomelessCountResponse.java
@@ -23,7 +23,7 @@ public class HomelessCountResponse {
       + " (locationTrackedAfter 보다 늦게 발생한 위치 추적 데이터만 조회)")
   private LocalDateTime locationTrackedAfter;
 
-  @Schema(description = "위치 추적 데이터가 존재하는 노숙인 수")
+  @Schema(description = "locationTrackedAfter 이후에 위치 추적 데이터가 존재하는 노숙인 수")
   private Long locationTrackedHomelessCount;
 
   @Schema(description = "외출중으로 감지된 노숙인 수")

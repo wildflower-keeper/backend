7655266e21a7af69707e73de162db2b635cc7885
yerimkim <kiel0103@naver.com>
2024-06-29 11:57:40 +0900

    feat: CORS uri 패턴 제한 제거

--- backend-api/src/main/java/org/wildflowergardening/backend/api/kernel/config/WebConfig.java
+++ backend-api/src/main/java/org/wildflowergardening/backend/api/kernel/config/WebConfig.java
@@ -19,8 +19,8 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/api/**")
-        .allowedOrigins("*")
+    registry.addMapping("**")
+        .allowedOrigins("http://localhost:8080")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
         .allowedHeaders("auth-token")
         .maxAge(3_600);

de1e0c81b568811a010250e884d13029dd9eed03
unknown <block0608@gmail.com>
2024-09-24 14:17:47 +0900

    fix : 긴급 상황 발생 시 위치 정보 필수 조건 삭제

--- backend-api/src/main/java/org/wildflowergardening/backend/api/wildflowergardening/application/dto/Location.java
+++ backend-api/src/main/java/org/wildflowergardening/backend/api/wildflowergardening/application/dto/Location.java
@@ -13,14 +13,14 @@ import java.math.BigDecimal;
 @Getter
 @Builder
 public class Location {
-    @NotNull(message = "위도가 null 일 수 없습니다.")
+
     @DecimalMax(value = "90", message = "위도 최댓값은 90 입니다.")
     @DecimalMin(value = "-90", message = "위도 최솟값은 -90 입니다.")
     @Digits(integer = 2, fraction = 6, message = "위도는 정수 두자리 이하, 소수점 6자리 이하로 지정 가능합니다.")
     @Schema(description = "위도")
     private BigDecimal lat;
 
-    @NotNull(message = "경도가 null 일 수 없습니다.")
+
     @DecimalMax(value = "180", message = "경도 최댓값은 180 입니다.")
     @DecimalMin(value = "-180", message = "경도 최솟값은 -180 입니다.")
     @Digits(integer = 3, fraction = 6, message = "경도는 정수 세자리 이하, 소수점 6자리 이하로 지정 가능합니다.")

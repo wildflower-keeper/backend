de838da756ab98f7d198e07d8f5a6637c700e89a
home <block0608@gmail.com>
2024-11-09 21:15:59 +0900

    feat: InOutStatus상태에 외박, 미확인 상태 값  추가

--- backend-core/src/main/java/org/wildflowergardening/backend/core/wildflowergardening/domain/InOutStatus.java
+++ backend-core/src/main/java/org/wildflowergardening/backend/core/wildflowergardening/domain/InOutStatus.java
@@ -1,13 +1,13 @@
 package org.wildflowergardening.backend.core.wildflowergardening.domain;
 
 public enum InOutStatus {
-  IN_SHELTER, OUT_SHELTER;
+    IN_SHELTER, OUT_SHELTER, OVERNIGHT_STAY, UNCONFIRMED;
 
-  public static InOutStatus from(String value) {
-    try {
-      return valueOf(value);
-    } catch (Exception e) {
-      throw new IllegalArgumentException("InOutStatus 에 " + value + "가 존재하지 않습니다.");
+    public static InOutStatus from(String value) {
+        try {
+            return valueOf(value);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("InOutStatus 에 " + value + "가 존재하지 않습니다.");
+        }
     }
-  }
 }

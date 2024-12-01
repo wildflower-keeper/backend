9835920b7d74f532e9942f5a167e487e690ae417
home <block0608@gmail.com>
2024-12-01 14:17:09 +0900

    feat: 모바일 앱에서 deviceId 저장 요청 api

---
+++ backend-api/src/main/java/org/wildflowergardening/backend/api/wildflowergardening/application/FCMService.java
@@ -0,0 +1,13 @@
+package org.wildflowergardening.backend.api.wildflowergardening.application;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class FCMService {
+
+}

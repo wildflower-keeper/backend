1ebf14dede0b6d4f81ae78205f5c6ab8a48b5fcf
home <block0608@gmail.com>
2025-01-11 21:41:08 +0900

    fix: 이미지 파일 형식 에러 발생 시 illegalException 발생

--- backend-api/src/main/java/org/wildflowergardening/backend/api/wildflowergardening/application/AwsS3Service.java
+++ backend-api/src/main/java/org/wildflowergardening/backend/api/wildflowergardening/application/AwsS3Service.java
@@ -52,7 +52,7 @@ public class AwsS3Service {
         if (isImageExtension(extension)) {
             return extension;
         }
-        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 형식의 파일" + fileName + ") 입니다.");
+        throw new IllegalArgumentException("이미지 파일만 가능합니다(" + fileName+")");
     }
 
     public void deleteFile(String fileName) {
